bd7cfd736a4ee877b8d28b353740ae8158fb95ae
jagige <123841712+jagige@users.noreply.github.com>
2026-05-27 14:51:22 -0300

    corrección de control
    se mueven los dos jugadores

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Netcode;
 
-public class playerController : MonoBehaviour
+public class playerController : NetworkBehaviour
 {
     private CharacterController _characterController;
     private Vector2 _input;
     private float _speed = 5f;
+    private PlayerInput _playerInput;
     //private float _yVelocity;
     //private float _gravity = -9.81f;
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
+        _playerInput = GetComponent<PlayerInput>();
+        _playerInput.enabled = false;
     }
    
 
@@ -19,6 +23,16 @@ public class playerController : MonoBehaviour
     {
         Vector3 Move = new Vector3(_input.x, 0, _input.y);
         _characterController.Move(Move * _speed * Time.deltaTime);
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        _playerInput.enabled = IsOwner;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        _playerInput.enabled = false;
     }
 
     public void OnMove(InputValue value)
